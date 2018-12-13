d9ca5b7c9194db98474d9aa48936c8c541a7435f
cyc <4296594@qq.com>
2018-12-13 16:13:11 +0800

    cyc提交项目架构更新

---
+++ Shopping/Areas/Manage/Controllers/CycController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopping.Areas.Manage.Controllers
+{
+    public class CycController : Controller
+    {
+        // GET: Manage/Cyc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Shopping/Controllers/CycController.cs
+++ Shopping/Controllers/CycController.cs
@@ -14,9 +14,39 @@ namespace Shopping.Controllers
             return View();
         }
         #region 个人信息
+        [HttpGet]
         public ActionResult Information()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Information(object obj) {
+            string to = Request["to"];
+            switch (to)
+            {
+                case "GetInformation":
+                    return GetInformation();
+            }
+            return View();
+        }
+        /// <summary>
+        /// 获取个人信息
+        /// </summary>
+        /// <param name="context"></param>
+        [HttpPost]
+        private ActionResult GetInformation()
+        {
+            //用户名
+            //会员
+            //昵称
+            //真实姓名
+            //性别
+            //身份证号码
+            //出生日期
+            //详细地址 省 市 区
+            //个人简介
+            return Json("");
         }
         #endregion
 
