1602436309c4456e81f7de59ba4cc55a18f1bba0
cyc <4296594@qq.com>
2018-12-16 09:57:56 +0800

    cyc提交 2018年12月16日09:57:52

--- Service/RoleGroupService.cs
+++ Service/RoleGroupService.cs
@@ -14,12 +14,14 @@ namespace Service
     {
         IUnitofWork unit;
         IRepository<RoleGroup> RGroupRep;
+        IRepository<RoleContact> contactRep;
         IRepository<Role> roleRep;
         public RoleGroupService(IUnitofWork unit) {
             this.unit = unit;
             RGroupRep = unit.Repository<RoleGroup>();
+            contactRep = unit.Repository<RoleContact>();
             roleRep = unit.Repository<Role>();
-        }s
+        }
 
         /// <summary>
         /// 查询所有用户权限管理组
@@ -30,13 +32,46 @@ namespace Service
             return RGroupRep.Get();
         }
         /// <summary>
+        /// 查询权限组对应的权限信息
+        /// </summary>
+        /// <returns></returns>
+        public decimal GetRoleInfo(int RGid) {
+            var roleDB = roleRep.Get();
+            var contactDB = contactRep.Get();
+
+            //需要的数据
+            //权限组对象
+            var RGroup = RGroupRep.Get(x => x.RG_no == RGid, "", null).FirstOrDefault();
+            //所关联的所有权限编号
+            IEnumerable<int> list =  contactRep.Get(x => x.RG_no == RGid, "", null).Select(x => x.RC_no);
+
+
+
+
+            //var obj = contactDB    .Join(roleDB, x => x.RG_no, y => y.R_no,(x, y) => new {
+            //    x.RC_no,
+            //    x.RG_no,
+            //    x.R_no
+            //}).Join(RGroupDB,x=>x.RG_no,y=>y.);
+            return default(decimal);
+        }
+        /// <summary>
         /// 新增管理员组
         /// </summary>
         /// <param name="name"></param>
         /// <returns></returns>
         public int InsertGroup(string name,List<int> roles)
         {
-            RGroupRep.Insert(new RoleGroup { RG_name = name });
+            var RGroup= RGroupRep.Get(null, "", x => x.OrderByDescending(y => y.RG_no)).FirstOrDefault();
+            int RGid = 1;
+            if (RGroup != null) {
+                RGid = RGroup.RG_no+1;
+            }
+            RGroupRep.Insert(new RoleGroup { RG_no = RGid, RG_name = name });
+            foreach (var rid in roles)
+            {
+                contactRep.Insert(new RoleContact() { RG_no = RGid, R_no = rid });
+            }
             return unit.Commit();
         }
     }
---
+++ Shopping/Areas/Manage/Models/RoleInfo.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping.Areas.Manage.Models
+{
+    public class RoleInfo
+    {
+        public RoleInfo(RoleGroup RGroup, List<Role> Roles) {
+            this.RGroup = RGroup;
+            this.Roles = Roles;
+        }
+
+        /// <summary>
+        /// 权限组
+        /// </summary>
+        public RoleGroup RGroup { get; }
+        /// <summary>
+        /// 所关联的权限集合
+        /// </summary>
+        public List<Role> Roles { get; }
+    }
+}
