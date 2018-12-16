f4205c573e067370f56241088d7ab4754ba3463a
cyc <4296594@qq.com>
2018-12-16 10:46:57 +0800

    cyc提交 2018年12月16日10:46:54

---
+++ Service/RoleContactService.cs
@@ -0,0 +1,29 @@
+﻿using Core.Repository;
+using Core.UnitofWork;
+using Entities;
+using IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class RoleContactService : IRoleContactService
+    {
+        IUnitofWork unit;
+        IRepository<RoleContact> contactRep;
+
+        public RoleContactService(IUnitofWork unit) {
+            this.unit = unit;
+            contactRep = unit.Repository<RoleContact>();
+        }
+
+
+        public IEnumerable<int> GetRidByGid(int gid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
